f3353c74a56756f0755f8ea284a0e6d81f3ad56e
daawch <caique.rdg.0@gmail.com>
2022-04-01 15:33:17 -0300

    [adding 'home' component]

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,9 @@
+export class UserLogin{
+    public id: number
+    public name: string
+    public usuario: string
+    public password: string
+    public token: string
+    public foto: string
+    public tipo: string
+}
